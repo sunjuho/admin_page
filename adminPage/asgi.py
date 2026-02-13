a637cac8d753bed22d929dd560ff96681e27abb6
sunjuho <sun_juho@naver.com>
2026-02-14 00:45:56 +0900

    DJANGO_SETTINGS_MODULE 수정

--- adminPage/asgi.py
+++ adminPage/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adminPage.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adminPage.settings.local')
 
 application = get_asgi_application()
--- adminPage/wsgi.py
+++ adminPage/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adminPage.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adminPage.settings.local')
 
 application = get_wsgi_application()
